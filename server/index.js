9346b3f237322fdcfc3574dfb92b1556e5b81569
Kevin Nguyen <nguyenkevin225@gmail.com>
2020-05-31 19:06:09 -0700

    retrieve message by SID

--- server/index.js
+++ server/index.js
@@ -4,29 +4,40 @@ require('dotenv').config()
 
 const express = require('express')
 const server = express() 
+const cors = require('cors')
 server.use(cors())
 
 const twilio = require('twilio')
-const accountSid = ''
-const authToken = ''
-const client = new Twilio(accountSid, authToken)
 
-client.messages.create({
-     body:"Hello from Node", 
-     to: '', 
-     from: ''
-}).then((message) => console.log(message.sid))
+const client = new twilio(accountSid, authToken)
+
+
 
 server.get('/', (req, res) => {
-     console.log(req)
+     //TODO - NOTE THAT THE CONTENT IS RETRIEVE BY QUERY --- not by req.body
+     console.log(req.query)
 
      // TODO - *CREATE* method is nested within the endpoint 
      client.messages.create({
-     body:"Hello from Node", 
-     to: '', 
-     from: ''
-     }).then((message) => console.log(message.sid))
+     body:`${req.query.textmessage}`, 
+     to: `${req.query.recipient}`, 
+     from: '8646687632'
+     }).then((message) => res.send(message.sid))
 })
+
+server.get('/get-message/:id', (req, res) => {
+     // TODO - use /:id and grab ID from req.params and insert to client.message() method 
+     //TODO - return the message back to frontend 
+     let id = req.params.id
+
+     client.messages(`${id}`)
+     .fetch()
+     .then((message) => {
+          // console.log('success', message.body)
+          res.send(message.body)
+     })
+})
+
 
 
 let port = process.env.PORT || 4000 
